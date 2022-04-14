4c68bb6a754f7669b0d4325088b850f51e5a0fc7
GuiFerreira01 <guilherme04ferreira03@gmail.com>
2022-04-14 00:01:02 -0300

    Fix merge errors

--- src/server.js
+++ src/server.js
@@ -2,10 +2,6 @@ require('dotenv').config({});
 const app = require('./app');
 const PORT = 3001;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> c1328ed4afcdf6b94043d79dcb3c0418b6ce463c
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`)
 })
--- src/services/connectAPI.js
+++ src/services/connectAPI.js
@@ -9,7 +9,6 @@ class ConnectMock {
       })
   };
 
-<<<<<<< HEAD
   async listUsers() {
     const requestConfig = {
       method: 'get',
@@ -27,32 +26,6 @@ class ConnectMock {
   }
 
   async listAddresses(data) {
-=======
-  async listUsers(data) {
-    try {
-      const requestConfig = {
-        method: 'get',
-        url: '/users',
-        params: {
-          page: data,
-          limit: 100
-        }
-      };
-
-      const UsersResponse = await this.axiosInstance.request(requestConfig)
-      const listUsers = UsersResponse.data;
-
-      
-
-      return listUsers;
-    } catch (error) {
-      return error
-    }
-  }
-
-  async listAddress(data) {
-    try {
->>>>>>> c1328ed4afcdf6b94043d79dcb3c0418b6ce463c
       const requestConfig = {
         method: 'get',
         url: `/users/${data}/address`,
@@ -62,19 +35,9 @@ class ConnectMock {
       const listAddress = AddressResponse.data;
 
       return listAddress;
-<<<<<<< HEAD
   }
 
   async listContacts(data) {
-=======
-
-    } catch (error) {
-      return error
-    }
-  }
-  async listContact(data) {
-    try {
->>>>>>> c1328ed4afcdf6b94043d79dcb3c0418b6ce463c
       const requestConfig = {
         method: 'get',
         url: `/users/${data}/contacts`,
@@ -84,16 +47,7 @@ class ConnectMock {
 
       const listContact = ContactResponse.data;
 
-<<<<<<< HEAD
       return listContact;
-=======
-
-      return listContact;
-
-    } catch (error) {
-      return error
-    }
->>>>>>> c1328ed4afcdf6b94043d79dcb3c0418b6ce463c
   }
 }
 
